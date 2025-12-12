2c778e06832ff9562dfb8bfafe8ef4e0607ec44d
vigadella <47326585+vigadella@users.noreply.github.com>
2025-12-12 22:57:19 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Бот работает! 🚖")
+
+bot.polling(none_stop=True)
